8fdd4089f871405ae65404a918a7e1218063537f
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-01-10 14:01:50 -0600

    8. refactor: enclosing course variable and array's parts into an unique object.

--- part1/exercises/1.-course-info/src/App.jsx
+++ part1/exercises/1.-course-info/src/App.jsx
@@ -1,28 +1,30 @@
 const App = () => {
   // Variables
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10
-    },
-  
-    {
-      name: "Using props to pass data",
-      exercises: 7
-    },
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10
+      },
     
-    {
-      name: "State of a component",
-      exercises: 14
-    }
-  ]
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      
+      {
+        name: "State of a component",
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content content={ parts }/>
-      <Total total = { parts }/>
+      <Header course={ course.name }/>
+      <Content content={ course.parts }/>
+      <Total total = { course.parts }/>
     </div>
   )
 }
