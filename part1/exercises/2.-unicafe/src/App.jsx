6f83a86f78624c0efc2cb9440dc238647c08df5a
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-01-16 17:29:08 -0600

    11. feat: creating handlers to manage statistics state. Binding them to buttons.

--- part1/exercises/2.-unicafe/src/App.jsx
+++ part1/exercises/2.-unicafe/src/App.jsx
@@ -8,20 +8,33 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  //Event handlers for opinion buttons
+  const handleGoodOpinions = () => {
+    setGood(value => value + 1);
+  }
+
+  const handleNeutralOpinions = () => {
+    setNeutral(value => value + 1);
+  }
+
+  const handleBadOpinions = () => {
+    setBad(value => value + 1);
+  }
+
   return (
     <div>
       <h1>Give Feedback</h1>
       <section>
-        <button>good</button>
-        <button>neutral</button>
-        <button>bad</button>
+        <button onClick={handleGoodOpinions}>good</button>
+        <button onClick={handleNeutralOpinions}>neutral</button>
+        <button onClick={handleBadOpinions}>bad</button>
       </section>
 
       <h2>Statistics</h2>
       <section>
-        <p>good<span></span></p>
-        <p>neutral<span></span></p>
-        <p>bad<span></span></p>
+        <p>good <span>{good}</span></p>
+        <p>neutral <span>{neutral}</span></p>
+        <p>bad <span>{bad}</span></p>
       </section>
     </div>
   )
