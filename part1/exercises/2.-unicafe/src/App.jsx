c746c5116b4fdc767eb2647db0692ca089e90d6b
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-01-16 17:14:57 -0600

    9. feat: creating app structure to unicafe exercise

---
+++ part1/exercises/2.-unicafe/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <section>
+        <button>good</button>
+        <button>neutral</button>
+        <button>bad</button>
+      </section>
+
+      <h2>Statistics</h2>
+      <section>
+        <p>good<span></span></p>
+        <p>neutral<span></span></p>
+        <p>bad<span></span></p>
+      </section>
+    </div>
+  )
+}
+
+export default App
