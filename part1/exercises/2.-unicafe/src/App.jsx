4c8af49d8bf9591f73f87553f7652d87cb0cf788
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-01-16 17:45:19 -0600

    14. refactor: creating new state for total amount of comments, setting it's value into button handlers.

--- part1/exercises/2.-unicafe/src/App.jsx
+++ part1/exercises/2.-unicafe/src/App.jsx
@@ -7,18 +7,22 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
   //Event handlers for opinion buttons
   const handleGoodOpinions = () => {
     setGood(value => value + 1);
+    setTotal(value => value + 1);
   }
 
   const handleNeutralOpinions = () => {
     setNeutral(value => value + 1);
+    setTotal(value => value + 1);
   }
 
   const handleBadOpinions = () => {
     setBad(value => value + 1);
+    setTotal(value => value + 1);
   }
 
   return (
@@ -35,7 +39,7 @@ const App = () => {
         <p>good <span>{good}</span></p>
         <p>neutral <span>{neutral}</span></p>
         <p>bad <span>{bad}</span></p>
-        <p>all <span>{good + neutral + bad}</span></p>
+        <p>all <span>{total}</span></p>
         <p>average <span></span></p>
         <p>positive <span></span></p>
       </section>
