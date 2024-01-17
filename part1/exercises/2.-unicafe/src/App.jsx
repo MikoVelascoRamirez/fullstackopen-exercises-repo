86574f5c70518ef2fa741ffedc333a53dec5507d
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-01-16 20:37:23 -0600

    15. feat: setting average state and modifying handler for good opinions. Also, adding a component for displaying it.

--- part1/exercises/2.-unicafe/src/App.jsx
+++ part1/exercises/2.-unicafe/src/App.jsx
@@ -8,11 +8,15 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
 
   //Event handlers for opinion buttons
   const handleGoodOpinions = () => {
-    setGood(value => value + 1);
-    setTotal(value => value + 1);
+    const goodOpinions = good + 1;
+    const totalOpinions = goodOpinions + neutral + bad; 
+    setGood(goodOpinions);
+    setTotal(totalOpinions);
+    setAverage((goodOpinions-bad) / totalOpinions);
   }
 
   const handleNeutralOpinions = () => {
@@ -40,7 +44,7 @@ const App = () => {
         <p>neutral <span>{neutral}</span></p>
         <p>bad <span>{bad}</span></p>
         <p>all <span>{total}</span></p>
-        <p>average <span></span></p>
+        <p>average <span>{average}</span></p>
         <p>positive <span></span></p>
       </section>
     </div>
