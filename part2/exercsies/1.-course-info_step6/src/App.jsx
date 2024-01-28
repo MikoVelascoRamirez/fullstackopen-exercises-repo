7adaa37a47bfd13779ab647308b26e1acff34dc9
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-01-28 17:05:46 -0600

    37. feat: changing previous main component of course-info creating a Course component and rendering parts by a map function.

---
+++ part2/exercsies/1.-course-info_step6/src/App.jsx
@@ -0,0 +1,82 @@
+const App = () => {
+  // Variables
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+
+      {
+        name: "State of a component II",
+        exercises: 8,
+        id: 4,
+      }
+    ],
+  };
+
+  return <Course course={course} />
+};
+
+const Course = ({course}) => {
+
+  console.log(course)
+  
+  const Header = ({ course }) => {
+    return <h1>{course}</h1>;
+  };
+
+  const Content = ({ content }) => {
+    console.log(content)
+    return (
+      <div>
+        {
+          content.map(part => <Part id={part.id} course={part}/>)
+        }
+      </div>
+    );
+  };
+
+  const Part = ({ course }) => {
+    console.log(course)
+    return (
+      <p>
+        {course.name} {course.exercises}
+      </p>
+    );
+  };
+
+  const Total = ({ total }) => {
+    return (
+      <p>
+        Number of exercises{" "}
+        {total[0].exercises + total[1].exercises + total[2].exercises}
+      </p>
+    );
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content content={course.parts} />
+      <Total total={course.parts} />
+    </div>
+  );
+};
+
+export default App;
