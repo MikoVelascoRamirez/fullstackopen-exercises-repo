779f499d366e105cb4962f85b72552cd15236d36
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-01-29 13:48:05 -0600

    41. refactor: creating Course component isolated of App.

--- part2/exercsies/1.-course-info_step6/src/App.jsx
+++ part2/exercsies/1.-course-info_step6/src/App.jsx
@@ -1,3 +1,5 @@
+import Course from "../components/Course";
+
 const App = () => {
   // Variables
   const courses = [
@@ -52,52 +54,6 @@ const App = () => {
       }
     </>
   )
-};
-
-const Course = ({course}) => {
-
-  console.log(course)
-  
-  const Header = ({ course }) => {
-    return <h1>{course}</h1>;
-  };
-
-  const Content = ({ content }) => {
-    console.log(content)
-    return (
-      <div>
-        {
-          content.map(part => <Part id={part.id} course={part}/>)
-        }
-      </div>
-    );
-  };
-
-  const Part = ({ course }) => {
-    console.log(course)
-    return (
-      <p>
-        {course.name} {course.exercises}
-      </p>
-    );
-  };
-
-  const Total = ({ total }) => {
-    console.log(total)
-    return (
-      <b>
-        Total of { total.reduce((result, {exercises}) => result += exercises, 0) } exercises
-      </b>
-    );
-  };
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content content={course.parts} />
-      <Total total={course.parts} />
-    </div>
-  );
 };
 
 export default App;
