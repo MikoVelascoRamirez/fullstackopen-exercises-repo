716f11ccd306cbf75e88dbbb1665d871ef8be986
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-02-12 15:56:03 -0600

    84. handling changing of country name state.

--- part2/exercsies/5.-countries_app/src/App.jsx
+++ part2/exercsies/5.-countries_app/src/App.jsx
@@ -5,12 +5,17 @@ const App = () => {
   const [country, setCountry] = useState("");
   const [result, setResult] = useState(null);
 
+  // Styate handling functions
+  const handleNameCountry = e => {
+    setCountry(e.target.value)
+  }
+
   return (
     <div>
-      <form>
+      <section>
         <label htmlFor="inp_country">Find countries </label>
-        <input type="text" id="inp_country" />
-      </form>
+        <input type="text" id="inp_country" onChange={handleNameCountry}/>
+      </section>
       <section>
         <p>Too many matches, specify another filter</p>
       </section>
