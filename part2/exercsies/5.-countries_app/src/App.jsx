529e73eda5c1303f02773d4258ae4fb945c6c293
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-02-13 15:51:10 -0600

    86. handling changing name country and binding to input text.

--- part2/exercsies/5.-countries_app/src/App.jsx
+++ part2/exercsies/5.-countries_app/src/App.jsx
@@ -1,26 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
-
   const [country, setCountry] = useState("");
   const [listOfCountries, setListOfCountries] = useState(null);
 
-  // Styate handling functions
-  const handleNameCountry = e => {
-    setCountry(e.target.value)
-  }
+  // State handling functions
+  const handleNameCountry = (e) => {
+    setCountry(e.target.value);
+  };
 
   return (
     <div>
       <section>
         <label htmlFor="inp_country">Find countries </label>
-        <input type="text" id="inp_country" onChange={handleNameCountry}/>
-      </section>
+        <input type="text" id="inp_country" onChange={handleNameCountry} />
+      </section>      
       <section>
         <p>Too many matches, specify another filter</p>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
