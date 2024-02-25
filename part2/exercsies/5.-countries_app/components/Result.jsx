d5f87beab4ad3b4051d9afceefdbd71202928819
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-02-25 17:18:42 -0600

    115. passing country get handler to list component.

--- part2/exercsies/5.-countries_app/components/Result.jsx
+++ part2/exercsies/5.-countries_app/components/Result.jsx
@@ -7,7 +7,7 @@ const Result = ({ result, getCountryHandler }) => {
   return typeof result?.msg === "string" ? (
     <Message msg={result.msg} color={result.color} />
   ) : Array.isArray(result) ? (
-    <ListCountries list={result} getCountry={getCountry} />
+    <ListCountries list={result} getCountry={getCountryHandler} />
   ) : result instanceof Object ? (
     <Country country={result} />
   ) : null;
