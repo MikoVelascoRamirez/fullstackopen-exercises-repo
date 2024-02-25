909bd5d641853015c00d3d01b3e7dab89a90814a
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-02-25 17:20:33 -0600

    116. feat: styling parragraph message.

--- part2/exercsies/5.-countries_app/components/Message.jsx
+++ part2/exercsies/5.-countries_app/components/Message.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const Message = ({msg}) => {
+const Message = ({msg, color}) => {
+
+  const messageStyle = {
+    color: !color ? 'black' : 'white',
+    width: 'fit-content',
+    fontSize: 18,
+    fontFamily: 'Helvetica',
+    paddingBlock: 10,
+    paddingInline: 8,
+    borderRadius: 3,
+    backgroundColor: color ?? 'white'
+  }
+
   return (
-    <p>{msg}</p>
+    <p style={messageStyle}>{msg}</p>
   )
 }
 
